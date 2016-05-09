243cece376829f4a33bc28908f4649fc486a1edf
harish-rzt <harish.v@razorthink.net>
2016-05-09 17:40:29 +0530

    agenda table and scroll issue fix

--- script/custom.js
+++ script/custom.js
@@ -1,34 +1,34 @@
 
 $(document).ready(function(){
-    
+
     $( window ).scroll(function() {
         $(".cn-home__header").toggleClass("fix-header", $(window).scrollTop() > $(window).height() - 140);
         $(".cn-home__wrapper").toggleClass("fix-header-responsive", $(window).scrollTop() > $(window).height());
         $(".cn-home__header").removeClass("expand");
     });
-    
+
     $(".cn-home__menu-button").click(function(){
         $(".cn-home__header").toggleClass("expand");
     });
-    
+
 //    $('a.cn-home__header__nav__link[href^="#"]').click(function() {
 //        $('html,body').animate({ scrollTop: $(this.hash).offset().top-50}, 700);
 //        return false;
 //        e.preventDefault();
 //    });
-    
+
     $(document).on("scroll", onScroll);
-    
+
     //smoothscroll
     $('.cn-home__header__nav__link[href^="#"]').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
-        
+
         $('.cn-home__header__nav__link').each(function () {
             $(this).removeClass('active');
         })
         $(this).addClass('active');
-      
+
         var target = this.hash,
             menu = target;
         $target = $(target);
@@ -39,7 +39,7 @@ $(document).ready(function(){
             $(document).on("scroll", onScroll);
         });
     });
-    
+
 });
 
 function onScroll(event){
@@ -47,7 +47,11 @@ function onScroll(event){
     $('.cn-home__header__nav__link').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos-150) {
+        if ((scrollPos > 200) && (scrollPos < 900)) {
+            $('.cn-home__header__nav__link').removeClass("active");
+            $('.cn-home__header__nav__link').first().addClass("active");
+        }
+        else if (refElement.position().top <= scrollPos-150) {
             $('.cn-home__header__nav__link').removeClass("active");
             currLink.addClass("active");
         }
